d9bcd163c031fd628588e902b27ec39f128debb4
zhiqiang <zhiqiang2033@gmail.com>
2025-02-12 09:39:43 +0800

    fix: cli

--- cli/main.go
+++ cli/main.go
@@ -16,11 +16,13 @@ var flags Flags
 type Flags struct {
 	FileName string
 	Dir      string
+	Force    bool
 }
 
 func init() {
 	flag.StringVar(&flags.FileName, "f", "archive.zip", "<filename>  Location of cli")
 	flag.StringVar(&flags.Dir, "C", ".", "Change to <dir> before processing remaining files")
+	flag.BoolVar(&flags.Force, "force", false, "Do you want to perform forced compression")
 }
 func main() {
 	flag.Parse()
@@ -30,37 +32,41 @@ func main() {
 		_ = os.RemoveAll(tempPath)
 	}()
 	if err := os.MkdirAll(tempPath, 0o755|os.ModeDir); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 	format := strings.Replace(path.Ext(flags.FileName), ".", "", 1)
 	file, err := os.Create(tempFileName)
 	if err != nil {
-		_ = os.Remove(flags.FileName)
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 	myArchive, err := archive.New(file, format)
 	if err != nil {
-		_ = os.Remove(flags.FileName)
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 	defer myArchive.Close()
-	if err := filepath.Walk(flags.Dir, func(path string, info os.FileInfo, err error) error {
+	if err := filepath.Walk(flags.Dir, func(path string, info os.FileInfo, err error) (rErr error) {
 		destination := strings.TrimPrefix(path, flags.Dir)
-		fmt.Println(fmt.Sprintf("adding: %s", destination))
-		return myArchive.Add(archive.File{
+		rErr = myArchive.Add(archive.File{
 			Destination: destination,
 			Source:      path,
 		})
+		if rErr == nil {
+			fmt.Println(fmt.Sprintf("Adding: %s ", destination))
+			return nil
+		}
+		if flags.Force {
+			return nil
+		}
+		return rErr
 	}); err != nil {
-		_ = os.Remove(flags.FileName)
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 	if err := os.Rename(tempFileName, flags.FileName); err != nil {
-		_ = os.Remove(flags.FileName)
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 }
